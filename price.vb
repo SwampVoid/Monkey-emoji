2453b7489fc514396f1627a821349183fda857fd
SwampVoid <138291932+SwampVoid@users.noreply.github.com>
2025-02-15 13:53:37 +1100

    Create price.vb

---
+++ price.vb
@@ -0,0 +1,13 @@
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButExit.Click
+        Stop
+    End Sub
+
+    Private Sub ButPrice_Click(sender As Object, e As EventArgs) Handles ButPrice.Click
+        Lable.Text = "$58.00"
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+End Class
